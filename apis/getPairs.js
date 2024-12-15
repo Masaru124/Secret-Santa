113a1be877a9c951f4bc5f63163d83d07f73fcf1
Masaru124 <masarukaze041@gmail.com>
2024-12-15 11:39:56 +0530

    verce

--- apis/getPairs.js
+++ apis/getPairs.js
@@ -1,11 +1,15 @@
 const fs = require('fs');
-const path = require('path');
 
-module.exports = async (req, res) => {
-  try {
-    const data = fs.readFileSync(path.join(__dirname, '../pairs.json'), 'utf8');
-    res.status(200).json(JSON.parse(data));
-  } catch (err) {
-    res.status(500).send("Error reading pairs.json");
-  }
+module.exports = (req, res) => {
+  fs.readFile('pairs.json', 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).send("Error reading pairs.json");
+    }
+    try {
+      const previousPairings = JSON.parse(data);
+      res.status(200).json(previousPairings);  // Return pairs from the file
+    } catch (e) {
+      res.status(200).json([]);  // Return empty array if pairs.json is empty or corrupted
+    }
+  });
 };
--- apis/savePairs.js
+++ apis/savePairs.js
@@ -1,12 +1,32 @@
 const fs = require('fs');
-const path = require('path');
 
-module.exports = async (req, res) => {
-  try {
-    const previousPairings = req.body.pairs;
-    fs.writeFileSync(path.join(__dirname, '../pairs.json'), JSON.stringify(previousPairings, null, 2));
-    res.status(200).send("Pairings saved successfully!");
-  } catch (err) {
-    res.status(500).send("Error writing to pairs.json");
-  }
+module.exports = (req, res) => {
+  const previousPairings = req.body.pairs;
+
+  // Read the current pairs.json to check if pairs already exist
+  fs.readFile('pairs.json', 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).send("Error reading pairs.json");
+    }
+
+    let currentPairs = [];
+    try {
+      currentPairs = JSON.parse(data);
+    } catch (e) {
+      currentPairs = [];
+    }
+
+    // If pairs already exist, don't overwrite them
+    if (currentPairs.length > 0) {
+      return res.status(400).send("Pairings already assigned. You can only start again if you want to generate new pairs.");
+    }
+
+    // If no pairs exist, save the new ones
+    fs.writeFile('pairs.json', JSON.stringify(previousPairings, null, 2), (err) => {
+      if (err) {
+        return res.status(500).send("Error writing to pairs.json");
+      }
+      res.status(200).send("Pairings saved successfully!");
+    });
+  });
 };
